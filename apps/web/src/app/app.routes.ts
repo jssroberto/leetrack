00dddc96c534391fcb099c8777d06735e51c78f2
Eduardo Talavera Ramos <eduardo.talavera245244@potros.itson.edu.mx>
2025-10-27 14:33:15 -0700

    set up colors, animation dependencies, fonts

--- apps/web/src/app/app.routes.ts
+++ apps/web/src/app/app.routes.ts
@@ -1,3 +1,28 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    loadComponent: () => import('./auth/auth').then((m) => m.Auth),
+    children: [
+      {
+        path: 'login',
+        loadComponent: () => import('./auth/login/login').then((m) => m.Login),
+      },
+      {
+        path: 'signup',
+        loadComponent: () => import('./auth/signup/signup').then((m) => m.Signup),
+      },
+      {
+        path: '',
+        redirectTo: 'login',
+        pathMatch: 'full',
+      },
+    ],
+  },
+  {
+    path: '',
+    redirectTo: 'login',
+    pathMatch: 'full',
+  },
+];

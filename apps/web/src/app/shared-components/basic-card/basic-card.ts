42609903a5b0f355abf875c17fb2077b3251773b
Eduardo Talavera Ramos <eduardotalaverar1@gmail.com>
2025-11-01 22:33:02 -0700

    better column division, using lucide-angular icons

--- apps/web/src/app/shared-components/basic-card/basic-card.ts
+++ apps/web/src/app/shared-components/basic-card/basic-card.ts
@@ -1,27 +1,30 @@
 import { CommonModule } from '@angular/common';
-import { Component, inject, Input } from '@angular/core';
-import { Icon } from './../../../services/icon-registry.service'
-import { DomSanitizer } from '@angular/platform-browser';
-import { LucideAngularModule, FileIcon } from 'lucide-angular';
+import { Component, Input } from '@angular/core';
+import { LucideAngularModule, FileIcon, AArrowDown, LucideIconData, LinkIcon, ChevronDown, SquareArrowOutUpRight } from 'lucide-angular';
 
 @Component({
   selector: 'app-basic-card',
+  standalone: true,
   imports: [CommonModule, LucideAngularModule],
   templateUrl: './basic-card.html',
-  styleUrl: './basic-card.css'
+  styleUrls: ['./basic-card.css']
 })
 export class BasicCard {
-  private sanitizer = inject(DomSanitizer);
-  readonly FileIcon = FileIcon;
+  private iconsMap: Record<string, LucideIconData> = {
+    'link': LinkIcon,
+    'chevronDown': ChevronDown,
+    'squareArrowOutUpRight': SquareArrowOutUpRight
+  };
 
   @Input() number!: string;
   @Input() title!: string;
   @Input() color!: string;
 
-  @Input() icon!: Icon;
-  @Input() size!: string;
+  @Input() iconName!: string;
+  @Input() size: string = '22';
+  @Input() iconWidth: string = '2';
 
-  sanitizeSvg(svg: string) {
-    return this.sanitizer.bypassSecurityTrustHtml(svg);
+  get icon() {
+    return this.iconsMap[this.iconName];
   }
 }

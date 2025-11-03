05ff6838c1e4574f1634722437183fe4364206d2
Roberto Garcia <jrobertogarcia16@gmail.com>
2025-11-02 18:48:13 -0700

    Initialize extension needed files

---
+++ apps/extension/.eslintrc.cjs
@@ -0,0 +1,12 @@
+[{
+	"resource": "/home/jssroberto/Documents/GitHub/leetrack/apps/extension/tsconfig.json",
+	"owner": "typescript",
+	"severity": 8,
+	"message": "Option '--resolveJsonModule' cannot be specified when 'moduleResolution' is set to 'classic'.",
+	"source": "ts",
+	"startLineNumber": 11,
+	"startColumn": 5,
+	"endLineNumber": 11,
+	"endColumn": 24,
+	"origin": "extHost1"
+}]
